dffcd005e752d8da482beb755dbc286a77805f66
jdsuarez <jdsuarez@fgs.co>
2024-12-25 22:41:49 -0500

    refactor: replace axios with fetch for API calls in HttpClient

--- 01 cleanClode-Dry/src/solid/02-open-close-c.ts
+++ 01 cleanClode-Dry/src/solid/02-open-close-c.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
+  //   async get(url: string) {
+  //     const { data, status } = await axios.get(url);
+  //     return { data, status };
+  //   }
   async get(url: string) {
-    const { data, status } = await axios.get(url);
-    return { data, status };
+    const response = await fetch(url);
+    const data = await response.json();
+    return { data, status: response.status };
   }
 }
